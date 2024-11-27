8b884f333d5f94753e6a7e67f2f40e045d199c0c
spideydev00 <alexspideydev@gmail.com>
2024-11-27 17:31:17 +0100

    Phone input field creation

--- lib/features/auth/presentation/pages/social_login.dart
+++ lib/features/auth/presentation/pages/social_login.dart
@@ -1,19 +1,46 @@
 import 'package:fantavacanze_official/core/constants/constants.dart';
+import 'package:fantavacanze_official/core/extensions/context_extension.dart';
 import 'package:fantavacanze_official/core/theme/colors.dart';
 import 'package:fantavacanze_official/core/theme/sizes.dart';
 import 'package:fantavacanze_official/core/pages/empty_branded_page.dart';
 import 'package:fantavacanze_official/features/auth/presentation/pages/standard_login.dart';
+import 'package:fantavacanze_official/features/auth/presentation/widgets/phone_input_field.dart';
 import 'package:fantavacanze_official/features/auth/presentation/widgets/promo_text.dart';
 import 'package:fantavacanze_official/features/auth/presentation/widgets/rich_text.dart';
 import 'package:fantavacanze_official/features/auth/presentation/widgets/social_button.dart';
 import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
-class SocialLoginPage extends StatelessWidget {
+class SocialLoginPage extends StatefulWidget {
   //login page route
   static get route =>
       MaterialPageRoute(builder: (context) => const SocialLoginPage());
 
   const SocialLoginPage({super.key});
+
+  @override
+  State<SocialLoginPage> createState() => _SocialLoginPageState();
+}
+
+class _SocialLoginPageState extends State<SocialLoginPage> {
+  late bool isPhoneButtonPressed;
+  late PhoneNumber phoneNumber;
+  late bool isValidPhoneNumber;
+  final phoneNumberController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    isPhoneButtonPressed = false;
+    isValidPhoneNumber = false;
+  }
+
+  @override
+  void dispose() {
+    phoneNumberController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,39 +50,103 @@ class SocialLoginPage extends StatelessWidget {
       widgets: [
         /* ----------------------------------------------------------- */
         //Google e Apple login
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SocialButton(
-              onPressed: () {},
-              socialName: 'Google',
-              isGradient: true,
-              bgGradient: ColorPalette.googleGradientsBg,
-              width: Constants.getWidth(context) * 0.25,
-              isIconOnly: true,
-            ),
-            const SizedBox(width: 10),
-            SocialButton(
-              onPressed: () {},
-              socialName: 'Apple',
-              isGradient: false,
-              bgColor: Colors.black,
-              width: Constants.getWidth(context) * 0.25,
-              isIconOnly: true,
-            ),
-          ],
+        Form(
+          key: formKey,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SocialButton(
+                onPressed: () {},
+                socialName: 'Google',
+                isGradient: true,
+                bgGradient: ColorPalette.googleGradientsBg,
+                width: Constants.getWidth(context) * 0.25,
+                isIconOnly: true,
+              ),
+              const SizedBox(width: 10),
+              SocialButton(
+                onPressed: () {},
+                socialName: 'Apple',
+                isGradient: false,
+                bgColor: Colors.black,
+                width: Constants.getWidth(context) * 0.25,
+                isIconOnly: true,
+              ),
+            ],
+          ),
         ),
         /* ----------------------------------------------------------- */
         //Login con telefono
         const SizedBox(height: 15),
-        SocialButton(
-          onPressed: () {},
-          socialName: "Telefono",
-          isGradient: false,
-          bgColor: ColorPalette.greenContainer,
-          width: Constants.getWidth(context) * 0.80,
-          isIconOnly: false,
-        ),
+        !isPhoneButtonPressed
+            ? SocialButton(
+                onPressed: () {
+                  setState(
+                    () {
+                      isPhoneButtonPressed = true;
+                    },
+                  );
+                },
+                socialName: "Telefono",
+                isGradient: false,
+                bgColor: ColorPalette.primary.withOpacity(0.92),
+                foregroundColor: ColorPalette.white,
+                width: Constants.getWidth(context) * 0.80,
+                isIconOnly: false,
+              )
+            : Column(
+                children: [
+                  SizedBox(
+                    width: Constants.getWidth(context) * 0.90,
+                    child: PhoneInputField(
+                      controller: phoneNumberController,
+                      onTrashIconTap: () {
+                        setState(
+                          () {
+                            isPhoneButtonPressed = false;
+                            isValidPhoneNumber = false;
+                            phoneNumberController.clear();
+                          },
+                        );
+                      },
+                      onInputChanged: (number) {
+                        phoneNumber = number;
+                      },
+                      onInputValidated: (isValidNumber) {
+                        setState(() {
+                          isValidPhoneNumber = isValidNumber;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: isValidPhoneNumber
+                        ? () {
+                            //supabase call
+                            //go to OTP page
+                          }
+                        : null, // Disabilitato se non valido
+                    style: context.elevatedButtonThemeData.style!.copyWith(
+                      elevation: const WidgetStatePropertyAll(1),
+                    ),
+                    child: const Text(
+                      "Richiedi Codice",
+                    ),
+                  ),
+                  if (!isValidPhoneNumber)
+                    Padding(
+                      padding: const EdgeInsets.only(top: ThemeSizes.md),
+                      child: Text(
+                        "Inserisci un numero valido.",
+                        style: context.textTheme.bodyLarge!.copyWith(
+                          color: ColorPalette.error,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                ],
+              ),
       ],
       newColumnWidgets: [
         /* ----------------------------------------------------------- */
