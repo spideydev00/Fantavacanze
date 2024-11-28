0dd596b1c03a36ab846e8a2d6ba84cf8e4ff2481
spideydev00 <alexspideydev@gmail.com>
2024-11-28 13:41:20 +0100

    Added URLSchemes and Supabase Initializaition

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:fantavacanze_official/core/secrets/app_secrets.dart';
 import 'package:fantavacanze_official/core/theme/theme.dart';
 import 'package:fantavacanze_official/features/auth/presentation/pages/social_login.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    anonKey: AppSecrets.supabaseKey,
+    url: AppSecrets.supabaseUrl,
+  );
+
   runApp(const MyApp());
 }
 
