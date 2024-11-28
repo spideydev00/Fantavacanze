1797deb4df783c029feb125f5ceb1dc2ca23f37b
spideydev00 <alexspideydev@gmail.com>
2024-11-28 16:27:51 +0100

    Google Sign In

---
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -0,0 +1,22 @@
+import 'package:fantavacanze_official/core/errors/failure.dart';
+import 'package:fantavacanze_official/core/errors/server_exception.dart';
+import 'package:fantavacanze_official/features/auth/data/remote_data_source/auth_remote_data_source.dart';
+import 'package:fantavacanze_official/features/auth/domain/entities/user.dart';
+import 'package:fantavacanze_official/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource authRemoteDataSource;
+  AuthRepositoryImpl({required this.authRemoteDataSource});
+
+  @override
+  Future<Either<Failure, User>> googleSignIn() async {
+    try {
+      final user = await authRemoteDataSource.signInWithGoogle();
+
+      return right(user);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -1,1 +1,11 @@
-class User {}
+class User {
+  final String id;
+  final String email;
+  final String name;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.name,
+  });
+}
