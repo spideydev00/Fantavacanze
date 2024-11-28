f57b8e40add98ba500b1b2318858d12af4df78c8
spideydev00 <alexspideydev@gmail.com>
2024-11-28 13:12:56 +0100

    remote_data_source initialization

---
+++ lib/features/auth/data/remote_data_source/auth_remote_data_source.dart
@@ -0,0 +1,11 @@
+abstract interface class AuthRemoteDataSource {
+  void signInWithGoogle();
+  void signInWithApple();
+  void signInWithPhone();
+  void signUpWithEmailPassword();
+  void signInWithEmailPassword();
+}
+
+// class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  
+// }
