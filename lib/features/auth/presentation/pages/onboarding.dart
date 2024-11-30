2e170a8fecf05aa0588d57b2f0faebc566f9494d
spideydev00 <alexspideydev@gmail.com>
2024-11-30 14:06:06 +0100

    OnBoarding changes

--- lib/features/auth/presentation/pages/onboarding.dart
+++ lib/features/auth/presentation/pages/onboarding.dart
@@ -1,5 +1,7 @@
 import 'package:fantavacanze_official/core/constants/constants.dart';
-import 'package:fantavacanze_official/features/auth/presentation/widgets/on_boarding_page.dart';
+import 'package:fantavacanze_official/core/theme/sizes.dart';
+import 'package:fantavacanze_official/features/auth/presentation/widgets/on_boarding_page_content.dart';
+import 'package:fantavacanze_official/features/auth/presentation/widgets/promo_text.dart';
 import 'package:fantavacanze_official/home.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -17,6 +19,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   //keeps track of which page we're on
   PageController controller = PageController();
   bool onLastPage = false;
+  int pageIndex = 0;
 
   @override
   void dispose() {
@@ -24,39 +27,100 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
     super.dispose();
   }
 
+  String _getBackgroundImage(int index) {
+    switch (index) {
+      case 0:
+        return "images/baddie-bg.jpg";
+      case 1:
+        return "images/social-enhance-bg.jpg";
+      case 2:
+        return "images/emotions-bg.jpg";
+      default:
+        return "images/baddie-bg.jpg";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
         title: Image.asset(
-          "images/logo-neon.png",
-          width: Constants.getWidth(context) * 0.30,
+          "images/logo-high-padding.png",
+          width: Constants.getWidth(context) * 0.60,
         ),
         toolbarHeight: 150,
       ),
       body: Stack(
         children: [
-          //page view
+          // Background with animation
+          AnimatedBuilder(
+            //listener (the controller, since it's updated on each scroll)
+            animation: controller,
+            //builder
+            builder: (context, child) {
+              return AnimatedSwitcher(
+                duration: const Duration(seconds: 1),
+                child: Container(
+                  key: ValueKey<int>(pageIndex),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(_getBackgroundImage(pageIndex)),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+
+          // Page view
           PageView(
             controller: controller,
             onPageChanged: (index) => {
-              setState(() {
-                onLastPage = (index == 2);
-              })
+              setState(
+                () {
+                  pageIndex = index;
+                  onLastPage = (index == 2);
+                },
+              )
             },
-            children: const [
-              OnBoardingPage(
-                pageDescription: "Dai una spinta alle tue vacanze",
-                imagePath: "images/summer-holiday-icon.png",
+            children: [
+              OnBoardingPageContent(
+                title: "Sfida Gli Amici.",
+                description:
+                    "Partecipa a delle leghe con il tuo gruppo di amici per decretare il king o la baddie dell'estate!",
+                ySpace: Constants.getHeight(context) * 0.4,
               ),
-              OnBoardingPage(
-                pageDescription: "Sfida i tuoi amici",
-                imagePath: "images/angry-heart.png",
+              Stack(
+                children: [
+                  // Promo Text at the top
+                  Positioned(
+                    top: Constants.getHeight(context) * 0.17,
+                    left: 0,
+                    right: 0,
+                    child: const PromoText(text: "Conosci Persone."),
+                  ),
+
+                  // Descriptive Text below
+                  Positioned(
+                    top: Constants.getHeight(context) * 0.17 +
+                        100 +
+                        16, // Adjust placement
+                    left: ThemeSizes.xxl,
+                    right: ThemeSizes.xxl,
+                    child: const Text(
+                      "Costruisci nuovi rapporti e rafforza le tue relazioni. Buttati, non avere paura di rischiare. YOLO!",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
               ),
-              OnBoardingPage(
-                pageDescription: "Prova i nostri giochi",
-                imagePath: "images/games-icon.png",
+              const OnBoardingPageContent(
+                title: "Vivi Momenti Unici.",
+                description:
+                    "Crea ricordi indimenticabili di cui poter discutere a distanza di anni. Goditi gli anni più belli.",
               ),
             ],
           ),
---
+++ lib/features/auth/presentation/widgets/on_boarding_page_content.dart
@@ -0,0 +1,36 @@
+import 'package:fantavacanze_official/core/theme/sizes.dart';
+import 'package:fantavacanze_official/features/auth/presentation/widgets/promo_text.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingPageContent extends StatelessWidget {
+  const OnBoardingPageContent({
+    super.key,
+    required this.title,
+    required this.description,
+    this.ySpace = 0,
+    this.alignment = MainAxisAlignment.center,
+  });
+
+  final String title;
+  final String description;
+  final double ySpace;
+  final MainAxisAlignment alignment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(ThemeSizes.xxl),
+      child: Column(
+        mainAxisAlignment: alignment,
+        children: [
+          SizedBox(height: ySpace),
+          PromoText(text: title),
+          Text(
+            description,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
